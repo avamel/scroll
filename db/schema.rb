c4d3038db716be39e7da7503da7cbd83801dc8d1
Pavel Zelenko <pasha.zel@gmail.com>
2013-07-19 11:25:20 +0300

    D

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130719074425) do
+ActiveRecord::Schema.define(:version => 20130719064947) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -136,9 +136,8 @@ ActiveRecord::Schema.define(:version => 20130719074425) do
   create_table "static_blocks", :force => true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.string   "identity_key"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
